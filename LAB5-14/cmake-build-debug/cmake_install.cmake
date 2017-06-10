b5af552bc56d77ca5636fa28d28a1711d9dc8828
Sorin Sebastian Mircea <sorynsoo@gmail.com>
2017-06-10 21:00:04 +0300

    Repeating for the final exam

--- LAB5-14/cmake-build-debug/cmake_install.cmake
+++ LAB5-14/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/so/Desktop/UBB/OOP2/LAB14
+# Install script for directory: /Users/so/Desktop/UBB/OOP2/LAB5-14
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/so/Desktop/UBB/OOP2/LAB14/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/so/Desktop/UBB/OOP2/LAB5-14/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
